7eb1312e7fcd9c7970b1f168d720003a6970d4d0
shamaz332 <shamazsaeed332@gmail.com>
2021-06-20 16:36:06 +0500

    binding element to attributes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
-<div id="app">
+  <div id="app">
     <div class="container">
-
       <div class="row">
         <div class="col-sm-12 text-center">
           <h1>Guest List</h1>
@@ -11,19 +10,27 @@
       <div class="row">
         <div class="col-sm-12">
           <div class="card">
-            <div class="card-header">
-    {{event.eventDate}}
+            <div class="card-header"  v-bind:class="formSubmittedClass">
+              {{ event.eventDate }}
             </div>
             <div class="card-block">
-              <h4 class="card-title">{{ event.eventTitle }}</h4><hr>
-              <p class="card-text">{{event.eventDescription}}</p>
-              <p class="card-text">Add your name to the guest list for exclusive offers:</p>
+              <h4 class="card-title" >{{ event.eventTitle }}</h4>
+              <hr />
+              <p class="card-text">{{ event.eventDescription }}</p>
+              <p class="card-text">
+                Add your name to the guest list for exclusive offers:
+              </p>
 
               <form class="form-inline" v-on:submit.prevent="submittedForm">
-                <input type="text" id="nameInput" class="form-control mb-2 mr-sm-2 mb-sm-0" placeholder="Jane Doe" v-model="newNameText">
-                <input type="submit" class="btn btn-primary"/>
+                <input
+                  type="text"
+                  id="nameInput"
+                  class="form-control mb-2 mr-sm-2 mb-sm-0"
+                  placeholder="Jane Doe"
+                  v-model="newNameText"
+                />
+                <input type="submit" class="btn btn-primary" v-bind:class="formSubmittedClass"/>
               </form>
-              {{newNameText}}
             </div>
           </div>
         </div>
@@ -33,21 +40,23 @@
         <div class="col-sm-12">
           <div class="card">
             <div class="card-header">
-              Attending ()
+              Attending ({{guestName.length}})
             </div>
             <div class="card-block">
-              <div>
-                <div class="card-success rounded name-box">
-
+              <div v-if="guestName.length > 0">
+                <div
+                  class="card-success rounded name-box"
+                  v-for="(name, index) in guestName"
+                  :key="index"
+                >
+                  {{ name }}
                 </div>
               </div>
-              <div>
+              <div v-else>
                 <h4>No names added yet...</h4>
-                {{guestName}}
               </div>
             </div>
           </div>
-
         </div>
       </div>
     </div>
@@ -55,9 +64,9 @@
 </template>
 
 <script>
-export default{
-name:"test",
-data: ()=>({
+export default {
+  name: "test",
+  data: () => ({
     event: {
       eventDate: "August 14th - 16th",
       eventTitle: "Summer Festival!",
@@ -66,19 +75,18 @@ data: ()=>({
     },
     newNameText: "",
     guestName: [],
-  }
-  )
-  ,
-  methods:
-  {
-
-    submittedForm:  function(){
-
-      this.guestName.push(this.newNameText)
-
-    }
-  }
-}
+    formSubmittedClass:""
+  }),
+  methods: {
+    submittedForm: function() {
+      if (this.newNameText !== "") {
+        this.guestName.push(this.newNameText);
+        this.newNameText = "";
+        this.formSubmittedClass="submitted"
+      }
+    },
+  },
+};
 </script>
 
 <style>
@@ -91,5 +99,7 @@ data: ()=>({
   margin: 10px;
   display: inline-block;
 }
-
+.submitted{
+  background-color: #5cb85c;
+}
 </style>
